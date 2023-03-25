3f55dd18673d05fa0f44046672d0775880b2aa42
Rocio ladavaz <ro.ladavaz@hotmail.com>
2023-03-25 15:46:59 +0800

    Entrega Final

--- main.js
+++ main.js
@@ -1,94 +1,89 @@
-//alimentos base de datos
 
-class Alimento {
-    constructor(nombre, familia, nutrientePrimario, caloria){
-        this.nombre = nombre;
-        this.familia = familia;
-        this.nutrientePrimario = nutrientePrimario;
-        this.caloria = caloria;
-    }
-}
+// FETCH
 
-const queso = new Alimento ("Queso","Lácteo", "Proteina", 300);
-const lenteja = new Alimento ("Lenteja", "Legumbre","Hidrato de carbono", 350);
-const garbanzo = new Alimento ("Garbanzo", "Legumbre","Hidrato de carbono", 340);
-const tomate = new Alimento ("Tomate", "Fruta", "Vitaminas, minerales y fibra", 50);
-const mayonesa = new Alimento ("Mayonesa", "Aderezo", "Grasas", 800);
-const aceite = new Alimento ("Aceite", "Grasa", "Acidos grasos", 900);
-const pan = new Alimento ("Pan", "Cereal", "Hidrato de carbono", 270);
-const banana = new Alimento ("Banana", "Fruta", "Fibra, vitaminas y minerales", 100);
-const manzana = new Alimento ("Manzana", "Fruta", "Fibra, vitaminas y minerales", 80);
-const pera = new Alimento ("Pera", "Fruta", "Fibra, vitaminas y minerales", 70);
-const yogur = new Alimento ("Yogur", "Lácteo", "Proteina", 70);
-const carne = new Alimento ("Carne", "Carne","Proteina", 300);
-const galletita = new Alimento ("Galletita de Agua", "Cereal", "Hidrato de carbono", 450);
-const papa = new Alimento ("Papa", "Verdura", "Hidrato de carbono", 90);
-const batata = new Alimento ("Batata", "Verdura", "Hidrato de carbono", 100);
-const pepino = new Alimento ("Pepino", "Verdura", "Vitaminas, minerales y fibra", 30);
+const arrayAlimentos = [];
+const alimentosBase = "json/alimentos.json";
+
+fetch(alimentosBase)
+    .then (respuesta => respuesta.json())
+    .then (data => { 
+        data.forEach (alimento => {
+            arrayAlimentos.push(alimento)
+            })
+        })
+    .catch (error => console.log(error))
+    .finally (()=> console.log ("proceso finalizado"));
 
 
-
-const arrayAlimentos = [queso, lenteja, garbanzo, tomate, mayonesa, aceite, pan, banana, manzana, pera, yogur, carne, galletita, papa, batata, pepino];
-
-//array de mi biblioteca
+//Array de mi biblioteca
 
 let biblioteca = [];
 
-//localStorage
+//LocalStorage
 
 if(localStorage.getItem("biblioteca")) {
     biblioteca = JSON.parse(localStorage.getItem("biblioteca"));
 }
 
 
-
-//BUSCADOR
+//Buscador
 
 const boton = document.getElementById("boton");
 const alimBuscado = document.getElementById("alimBuscado");
 const resultado = document.getElementById("resultado");
 
-//función buscador
+//Función buscador
 
 const buscador = () =>{
     resultado.innerHTML = '';
-    const texto = alimBuscado.value.toLowerCase();
-    for (let arrayAlimento of arrayAlimentos){
-        let nombre = arrayAlimento.nombre.toLowerCase();
-        if (nombre.indexOf(texto) !== -1){
-            resultado.innerHTML += `
-            <div class = "card w-50" >
-                <div class = "card-body">
-                    <h2> ${arrayAlimento.nombre} </h2>
-                    <p> Tipo de alimento: ${arrayAlimento.familia} </p>
-                    <p> Nutriente Primario: ${arrayAlimento.nutrientePrimario} </p>
-                    <p> Cada 100gr de alimento aporta: ${arrayAlimento.caloria} kcal </p>
-                    <button type="submit" class="btn btn-outline-dark" id = "boton${arrayAlimento.nombre}" >Agregar a mi biblioteca</button>
-                </div>
-            </div>`  
-            const boton = document.getElementById(`boton${arrayAlimento.nombre}`);
-            boton.addEventListener("click", () => {
-                agregarBiblioteca(arrayAlimento.nombre);
-            })   
+    if (alimBuscado.value === ""){
+        swal.fire("No ha escrito nada, ingrese el alimento que desea conocer");
+    }else{
+        const texto = alimBuscado.value.toLowerCase();
+        for (let arrayAlimento of arrayAlimentos){
+         let nombre = arrayAlimento.nombre.toLowerCase();
+         if (nombre.indexOf(texto) !== -1){
+             resultado.innerHTML += `
+                <div class = "card w-50" >
+                  <div class = "card-body">
+                      <h2> ${arrayAlimento.nombre} </h2>
+                      <p> Tipo de alimento: ${arrayAlimento.familia} </p>
+                     <p> Nutriente Primario: ${arrayAlimento.nutrientePrimario} </p>
+                     <p> Cada 100gr de alimento aporta: ${arrayAlimento.caloria} kcal </p>
+                     <button type="submit" class="btn btn-outline-dark" id = "boton${arrayAlimento.nombre}" >Agregar a mi biblioteca</button>
+                  </div>
+                </div>`  
+                const boton = document.getElementById(`boton${arrayAlimento.nombre}`);
+                boton.addEventListener("click", () => {
+                    agregarBiblioteca(arrayAlimento.nombre);
+                 })   
+            } 
         }
-    }  
-    if (resultado.innerHTML === '' ){
-        resultado.innerHTML = `<p>Este alimento no se encuentra en la base de datos</p>`
-    }    
+        if(resultado.innerHTML === '' ){
+            swal.fire("Este alimento no se encuentra en la base de datos");
+        }
+    }
 }
 boton.addEventListener("click", buscador);
 
 
-//funcion agregar a biblioteca
+//Prevención default busqueda
+const formulario = document.getElementById("formulario");
+formulario.addEventListener ("submit", (e) => {
+    e.preventDefault();
 
-let mensaje = document.getElementById ("mensaje");
+})
+
+
+//Función agregar a biblioteca
+
 const agregarBiblioteca = (nombre) => {
     const alimEnbiblio = biblioteca.find(alimento => alimento.nombre === nombre);
     if (alimEnbiblio){
-        mensaje.innerHTML = `<p class= "mensaje">Este alimento ya se encuentra en la biblioteca </p>`;
+        swal.fire("Este alimento ya se encuentra en la biblioteca");
     } else{ 
         const alim = arrayAlimentos.find(alimento => alimento.nombre === nombre);
-    biblioteca.push(alim);
+        biblioteca.push(alim);
     }
     
     console.log (biblioteca);
@@ -97,7 +92,7 @@ const agregarBiblioteca = (nombre) => {
     localStorage.setItem("biblioteca", JSON.stringify(biblioteca));
 }
 
-//mostrar biblioteca
+//Mostrar biblioteca
 
 const contenedorBiblio = document.getElementById("contenedorBiblio");
 const botonBiblio = document.getElementById ("botonBiblio");
@@ -105,7 +100,7 @@ const botonBiblio = document.getElementById ("botonBiblio");
 botonBiblio.addEventListener("click", () => {
     verBiblio();
 })
-//funcion para ver la biblioteca
+//Función para ver la biblioteca
 
 const verBiblio = () => {
     contenedorBiblio.innerHTML = "";
@@ -132,7 +127,7 @@ const verBiblio = () => {
     });
 }
 
-// función que elimina alimentos de la biblioteca:
+// Función que elimina alimentos de la biblioteca:
 
 const eliminoAlimento = (nombre) => {
     const alimGuardado = biblioteca.find (alimento => alimento.nombre === nombre);
@@ -152,7 +147,7 @@ limpiarBiblio.addEventListener("click", () => {
     clean();
 })
 
-//función limpiar biblio
+//Función limpiar biblio
 const clean = () => {
     biblioteca = [];
     verBiblio();
